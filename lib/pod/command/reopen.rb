6ad63a6a5ff830744511c494ba35256613921171
dacaiguoguo <dacaiguoguo@163.com>
2019-11-14 11:16:25 +0800

    re open workspace

--- lib/pod/command/reopen.rb
+++ lib/pod/command/reopen.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 module Pod
   class Command
     class Reopen < Command
-
       self.summary = 'Reopen the workspace'
       self.description = <<-DESC
         Close and opens the workspace in Xcode. If no workspace found in the current directory,
@@ -15,33 +16,33 @@ module Pod
 
       def validate!
         super
-        raise Informative, "No xcode workspace found" unless @workspace
+        raise Informative, 'No xcode workspace found' unless @workspace
       end
 
       def run
-        ascript = <<-STR.strip_heredoc
-tell application "Xcode"
-        set docs to (document of every window)
-        repeat with doc in docs
-            if class of doc is workspace document then
-                set docPath to path of doc
-                if docPath begins with "#{@workspace}" then
-                    log docPath
-                    return
-                end if
-            end if
-        end repeat
-end tell
-STR
-# puts ascript
+        ascript = <<~STR.strip_heredoc
+          tell application "Xcode"
+                  set docs to (document of every window)
+                  repeat with doc in docs
+                      if class of doc is workspace document then
+                          set docPath to path of doc
+                          if docPath begins with "#{@workspace}" then
+                              log docPath
+                              close doc
+                              return
+                          end if
+                      end if
+                  end repeat
+          end tell
+        STR
         `osascript -e '#{ascript}'`
-        # `open "#{@workspace}"`
+        `open "#{@workspace}"`
       end
 
       private
 
       def find_workspace_in(path)
-        path.children.find {|fn| fn.extname == '.xcworkspace'} || find_workspace_in_example(path)
+        path.children.find { |fn| fn.extname == '.xcworkspace' } || find_workspace_in_example(path)
       end
 
       def find_workspace_in_example(path)
